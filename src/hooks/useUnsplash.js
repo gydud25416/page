a011f1b44cbe25b58836a55e4da0096c37a9e771
gydud25416 <152379377+gydud25416@users.noreply.github.com>
2024-09-24 17:26:55 +0900

    chore: API호출 경로 오류 수정

--- src/hooks/useUnsplash.js
+++ src/hooks/useUnsplash.js
@@ -4,20 +4,20 @@ import { useEffect, useState } from "react";
 export default function useUnplash(url) {
     const [data, setData] = useState([]);
     useEffect(() => {
-        const bg_img = localStorage.getItem('bgImg')
-        if (bg_img === null) { // 로컬스토리지에 이미지가 없을때만 가져오기
-            axios.get(url)
-                .then(res => {
+        const bg_img = localStorage.getItem('bgImg');
+        axios.get(url)
+            .then(res => {
+                if (!bg_img) { // 로컬스토리지에 이미지가 없을때만 가져오기
                     setData(res.data.urls.regular);
-                    localStorage.setItem('bgImg', res.data.urls.regular)
-                })
-                .catch(error => {
-                    if(error.response.status === 404){
-                        localStorage.removeItem('bgImg');
-                    }
-                    console.error(error);
-                })
-        }
+                    localStorage.setItem('bgImg', res.data.urls.regular);
+                }
+            })
+            .catch(error => {
+                if (error.response.status > 400 && error.response.status < 500) {
+                    localStorage.removeItem('bgImg');
+                }
+                console.error(error);
+            })
     }, [url])
     return data
 }
